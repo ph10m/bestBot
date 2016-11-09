712cd98811da5d435da34e9b035879741ee7d37e
lollabies <erl1sharpy@gmail.com>
2016-11-09 20:13:00 +0100

    Skrevet arbitrator
    Skrevet ferdig arbitrator med en funskjon choose_action

--- arbitrator.py
+++ arbitrator.py
@@ -1,17 +1,33 @@
-from motob import Motob as MOTOR
+from random import uniform
 
 class Arbitrator:
+    def __init__(self, bbcon):
+        self.bbcon = bbcon  # En pointer til bbcon-objektet
+        recommendations = []
+        
+    def choose_action(self):
+        # hent ut en tilfeldig aksjon fra lista med en vektet sannsynlighet
+        recommendations = self.bbcon.recommendations
+        r_sum = 0
+        for recommendation in recommendations:
+            if recommendation[1] == 1:  # Returnerer recommendationen dersom den har prioritet 1
+                return recommendation
+            else:
+                r_sum += recommendation[1]
+        rand_num = uniform(0, r_sum)
+        print(rand_num)
+        for recommendation in recommendations:
+            if rand_num <= recommendation[1]:
+                return recommendation
+            else:
+                rand_num -= recommendation[1]
+
+"""
+class Bbcon:
     def __init__(self):
-        self.current_motor_rec = None
-        self.motor = MOTOR()
-        
-    def choose_action(self, sensors):
-        # hent ut den høyst prioriterte sensoren ut fra lista
-        # og lag så en "motor recommendation"
-        sensor_type,value = max(sensors, key=itemgetter(1))
-        self.motor.run(sensor_type,value)
-        self.wait()
-        
-    def wait(self):
-        # implementer threading?
-        pass
+        self.recommendations = [["a", 0.5], ["b", 0.7], ["c", 0.9], ["d", 1]]
+        arb = Arbitrator(self)
+        print(arb.choose_action())
+
+if __name__ == "__main__":
+    bbcon = Bbcon()"""
