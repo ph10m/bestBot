e19f266f28a8770e44711578f06e6cb3d770d767
Maria Soleim <maria@soleim.com>
2016-11-10 12:18:58 +0100

    Some work lol

--- bbcon.py
+++ bbcon.py
@@ -7,49 +7,62 @@ m = motor()
 def main():
     button.wait_for_press()
     m.forward(speed=0.5, dur=0.5)
-    m.backward(speed=0.5, dur=0.5)    
+    m.backward(speed=0.5, dur=0.5)
 
 main()
 
 class BBCON:
     def __init__(self):
-        self.ARB = ARB()
-    
+        self.ARB = ARB(self)
+
         self.behaviors = []         # all behaviors
         self.active = []            # active behaviors
         self.inactive = []          # inactive behaviors
-        
+
         self.sensobs = []           # sensor objects
         self.motobs = []            # motor object(s), useless?!?!
+        self.recommendations = []
 
         self.timestamp = 0
         self.motor_runtime = 0.5
-        
+
     def add_behavior(self,b):
         self.behaviors.append(b)
-    
+        self.inactive.append(b)     # all behaviors are inactive be default
+
     def add_sensob(self,s):
         self.sensobs.append(s)
-    
+
     def activate(self,b):
         if b in self.inactive_behaviors and b in self.behaviors:
             self.active.append(b)
             self.inactive.remove(b)
-    
+
     def deactivate(self,b):
         if b in self.active_behaviors and b in self.behaviors:
             self.inactive.append(b)
             self.active.remove(b)
-            
+
     def run(self):
-        current_data = []
-        for sensor in self.sensobs:
-            current_data.append(sensor, sensor.update()) # returnerer kun verdi
-            # oppdaterer current_data med prioritet og verdi til enhver sensor
-            # denne skal være en tuppel med (sensortype, verdi)
-        self.ARB.choose_action(current_data)
-        self.reset()
-    
+
+        # Update all sensobs
+        for sensob in self.sensobs:
+            sensob.update()
+
+        # Update all behaviors
+        for behavior in self.behaviors:
+            
+
+        recommendations = arbitrator.choose_action(self.sensobs)
+
+        # Update motobs
+
+        # Wait
+
+        # Reset sensobs
+
+
+
     def reset(self):
         self.timestamp += 1
         for sensor in self.sensobs:
